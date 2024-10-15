2acff7ac54628621d29ab69a60b6e45501f390a6
GitHubConnoisseur <B00162732@mytudublin.ie>
2024-10-15 13:15:05 +0100

    Unity2-Commit1

---
+++ Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+    private float startDelay = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Invote SpawnRandomBall with a startDelay 
+        Invoke("SpawnRandomBall", startDelay);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {   
+        //Generate a random ball index
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+        //start with a random value
+        float spawnInterval = Random.Range(3.0f, 5.0f);
+
+        //Invoking SpawnRandomBall with spawnInterval which is recalculated everytime Invoke is used 
+        Invoke("SpawnRandomBall", spawnInterval);
+        
+    }
+
+}
